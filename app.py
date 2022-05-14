e7fac749cd20eea6f39c6f360a64f988d5a6ddca
lukeboi <lukefarritor@gmail.com>
2022-05-13 23:56:17 -0500

    added json responses and other stuff

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import openai
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -13,11 +14,14 @@ app = Flask(__name__)
 openai.api_key_path = "./key.txt"
 
 prompt = "A man has a book, a chair, a pencil and a box. Written in the style of an action-packed performance, what does he do?"
-next = ["\n", "\nNext ", "\nNext, ", "\nAfterwards ", "\nAfterwards, ", "\nThen ",
-        "\nThen, "]  # possible next sequences to prompt gpt3
-script = [prompt + next[0]]  # start the script with the prompt and a newline
+# next = ["\n", "\nNext ", "\nNext, ", "\nAfterwards ", "\nAfterwards, ", "\nThen ",
+#         "\nThen, "]  # possible next sequences to prompt gpt3
+# script = [prompt + next[0]]  # start the script with the prompt and a newline
+
+script = [prompt]
 
 filename = "scripts/" + str(int(time.time())) + ".txt"
+
 
 def script_as_str():
     return ' '.join(script)
@@ -27,36 +31,36 @@ def script_as_html():
     return "<p>" + '<br>'.join(script) + "</p>"
 
 
-def create_prompt():
-    return script_as_str() + random.choice(next)
-
-def write_script_to_file():
-    with open(filename, 'w') as file:
-        file.write(script_as_str())
+def write_response_to_file(response):
+    with open(filename, 'a') as file:
+        file.write(str(response))
 
 
 @app.route("/")
 def main():
-    while True:
-        prompt = create_prompt()
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=512,
-            top_p=1,
-            frequency_penalty=2,
-            presence_penalty=0.5
-        )
-        text = response["choices"][0]["text"]
-        text_no_whitespace = re.sub(r"\W", "", text)
+    prompt = script_as_str() + request.args.get('prompt')
+    gpt_response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=2,
+        presence_penalty=0.5
+    )
+    text = gpt_response["choices"][0]["text"]
+    text_no_whitespace = re.sub(r"\W", "", text)
 
-        if len(text_no_whitespace) > 0:  # if the completion isn't blank
-            break
+    script.append(gpt_response["choices"][0]["text"])
+    print(gpt_response["choices"][0]["text"])
 
-    script.append(response["choices"][0]["text"])
-    print(response["choices"][0]["text"])
+    response = {
+        "prompt": prompt,
+        "script-html": script_as_html(),
+        "time": time.time(),
+        "response": gpt_response["choices"][0]["text"]
+    }
 
-    write_script_to_file()
+    write_response_to_file(response)
 
-    return script_as_html()  # send the user the whole generated script
+    return response  # send the user the whole generated script
